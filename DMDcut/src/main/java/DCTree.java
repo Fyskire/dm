b031ae0ca438fb37f7cc605ce61edd6ff2202cc9
Fyskire <Yreinhar@students.uni-mainz.de>
2016-01-19 00:07:00 +0100

    Input fertig

--- DMDcut/src/main/java/DCTree.java
+++ DMDcut/src/main/java/DCTree.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 
 public class DCTree {
 	
-	ArrayList<Node> v; //entspricht v aus dem paper. speichert ob checked oder nicht, index +1 entspricht namen der Knoten
-	ArrayList<ArrayList<WeightedEdge>> ew; //entpsricht e und w aus dem paper.
+	ArrayList<Node> v = new ArrayList<Node>(); //entspricht v aus dem paper. speichert ob checked oder nicht, index +1 entspricht namen der Knoten
+	ArrayList<ArrayList<WeightedEdge>> ew = new ArrayList<ArrayList<WeightedEdge>>(); //entpsricht e und w aus dem paper.
 	BufferedWriter br;
-	ArrayList<WeightedEdge> edgeList;
+	ArrayList<WeightedEdge> edgeList = new ArrayList<WeightedEdge>();
 	
 	public void input(String path){
+		ArrayList<WeightedEdge> zwischenspeicher = new ArrayList();
 		BufferedReader br = null;
 		String line = "";
 		String splitBy = " ";
@@ -21,7 +22,9 @@ public class DCTree {
 			br = new BufferedReader(new FileReader(path));
 			
 			//Einlesen von Knoten in v
-			String[] data = line.split("*Verticles ");
+//			while (!(line = br.readLine()).equals("*Vertices")){}
+			line = br.readLine();
+			String[] data = line.split("Vertices ");
 			int vSize = Integer.parseInt(data[1]);
 			v = new ArrayList<Node>();
 			
@@ -37,13 +40,25 @@ public class DCTree {
             while ((line = br.readLine()) != null){
             	
             	String[] data2 = line.split(splitBy);
-            	WeightedEdge w = new WeightedEdge(1.0, new Tuple(Integer.valueOf(data[0]), Integer.valueOf(data[1])));
+            	WeightedEdge w = new WeightedEdge(1.0, new Tuple(Integer.valueOf(data2[0]), Integer.valueOf(data2[1])));
             	edgeList.add(w);
             }
-            
+            int eSize = edgeList.size();
             //edgeListe durchgehen und ew entsprechend hinzufuegen
-            for(int x = 0; x < vSize; x++){
-            	
+            for(int i = 0; i < vSize; i++){//Für jeden existierenden Knoten
+            	zwischenspeicher.clear(); //Leere den zwischenspeicher
+            	for(int j = 0; j < eSize; j++){ //Alle Eintraege von edgeList durchgehen
+            		WeightedEdge el = edgeList.get(j); //Hole Kante aus der Liste
+            		int x = el.edge.x;//Merke die beiden durch die Kante verbunden Knoten
+            		int y = el.edge.y;
+            		if((x == (i+1))){ //Entspricht x oder y dem aktuellen Knoten, so speichere die Kante im Zwischenspeicher
+            			zwischenspeicher.add(el);
+            		}
+            		if((y == (i+1))){
+            			zwischenspeicher.add(el);
+            		}
+            	}
+            	ew.add(zwischenspeicher);//Fuege den Zwischenspeicher der ew Liste hinzu (Index ist Name des Knoten, analog zu Liste v)
             }
             
 //            Ende des aufbaus
@@ -59,14 +74,14 @@ public class DCTree {
                     e.printStackTrace();
 					}
             }
-        }
-
-        System.out.println("Input done");
+		}
+        System.out.println("Input done. ew size is: " + ew.size());
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DCTree t = new DCTree();
+		t.input("karate.paj");
 	}
 
 }
